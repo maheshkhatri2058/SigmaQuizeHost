cd9dd58c9604c5f719df284f9358b68562b70ab1
Mahesh <maheshkhatri2058@gmail.com>
2025-07-18 21:56:36 +0530

    new files added

--- src/UI/Landingpage.jsx
+++ src/UI/Landingpage.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 const Landingpage = () => {
   return (
     <>
-        <section className="bg-[url('bg.jpg')] bg-cover bg-center h-screen p-4 flex flex-col justify-center align-middle items-center gap-2">
+        <section className="bg-[url('/bg.jpg')] bg-cover bg-center h-screen p-4 flex flex-col justify-center align-middle items-center gap-2">
         <h2 className='text-center align-middle text-white text-3xl font-extralight'>Welcome to <span className='text-orange-600 font-semibold'> Sigma Quize </span> Competition <br></br>Host By MCA Departement</h2>
         <button className='border-[1px] rounded-2xl border-solid border-white bg-orange-700 p-2 text-white'>Click Here To Start The Quize</button>
         </section>
