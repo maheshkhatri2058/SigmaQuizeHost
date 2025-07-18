2a587c3def1c9fa06a164c4fb21fac0ba5fe6db5
Mahesh <maheshkhatri2058@gmail.com>
2025-07-18 21:46:35 +0530

    new files added

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Navbar from './UI/Navbar';
+import Landingpage from './UI/Landingpage';
+function App() {
+
+  return (
+    <>
+    <Navbar></Navbar>
+    <Landingpage/>
+    </>
+  )
+}
+
+export default App
---
+++ src/UI/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+const Navbar = () => {
+  return (
+    <div className='bg-blue-600 text-2xl text-white p-2'>
+    <navbar class="">
+        <h2 className='p-2'>SigmaGroupMCA</h2>
+    </navbar>
+      
+    </div>
+  )
+}
+
+export default Navbar
