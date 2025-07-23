d30ae4011887146598d06678c202e32b11dfeb25
Mahesh <maheshkhatri2058@gmail.com>
2025-07-23 11:10:32 +0530

    new files added

--- src/UI/Landingpage.jsx
+++ src/UI/Landingpage.jsx
@@ -6,9 +6,9 @@ const Landingpage = () => {
     <>
     <Navbar/>
         <section className="bg-[url('/bg.jpg')] bg-cover bg-center h-screen p-4 flex flex-col justify-center align-middle items-center gap-2">
-        <h2 className='text-center align-middle text-white text-3xl  font-semibold'>Welcome to <span className='text-orange-600 font-semibold'> TECHTUSSLE </span>2025 Host By MCA Departement</h2>
+        <h2 className='text-center align-middle text-white text-3xl  font-semibold'>Welcome to <span className='text-orange-600 font-semibold'> TECHTUSSLE </span>-25<br></br> Host By MCA Departement</h2>
         <p className='text-[14px] text-white font-[cursive]'>
-        Welcome to QuizWhiz – a thrilling quiz competition hosted as part of AVISHKARAM at Acharya Institute of Technology, Bangalore.
+         QuizWhiz – a thrilling quiz competition hosted as part of AVISHKARAM at Acharya Institute of Technology, Bangalore.
          
         </p>
 
--- src/UI/Login.jsx
+++ src/UI/Login.jsx
@@ -25,31 +25,57 @@ const Login = () => {
   return (
     <div>
     <Navbar/>
-    <section className='flex flex-col justify-center align-middle p-10 h-[550px] '>
-    <div className='flex flex-col justify-center align-middle items-center'>
-        <h2 className='text-2xl font-semibold text-amber-700'>Best Up Luck!!</h2>
-        <p>Please Fill Your Data Carefully</p>
+    <section className='flex justify-around align-middle py-20 h-[550px] '>
+    <div className='w-[50%]'>
+      
+      <h2 className='text-3xl font-bold text-yellow-700'> Welcome Quizwizz-2025!!!!!</h2>
+      <p className='text-2xl font-semibold text-red-500'>
+      Basic Rules For QuizeWizzz
+      </p>
+     <h2>1. You have a total of 50 minutes to complete 50 questions.</h2>
+     <h2 className='text-red-600'>
+2.Please close all other browser tabs before starting the quiz.
+
+     </h2>
+     <h2 className='text-red-600 font-semibold'>
+3.Switching between tabs is strictly prohibited. Doing so will result in automatic quiz termination and your answers will not be submitted.
+
+     </h2>
+     <h2 className='text-red-600'>
+4.Do not open any external applications during the quiz. This may lead to disqualification and immediate exit from the quiz.
+
+     </h2>
+
+
+
+      
     </div>
-        <form className='flex flex-col justify-center align-middle items-center p-10' onSubmit={getinfo}>
+    <div>
+    <h2 className='text-[cursive] text-yellow-600 text-xl font-semibold'>Please Enter your deatils Carefully!!</h2>
+     <form className='flex flex-col justify-center align-middle items-center' onSubmit={getinfo}>
             
             <div className='p-2'>
             <label className='text-xl font-extralight '>Enter Group Name:</label><br></br>
             <input 
             type="text" name="username" 
             onChange={(e) => setUsername(e.target.value)}
-            required className='border-2  border-slate-400 border-solid rounded-md p-1.5 px-2'   />
+            required className='border-2  border-slate-400 border-solid rounded-md p-1.5 px-8'   />
             </div>
             <div className='p-2'>
             <label  className='text-xl font-extralight'>AUID No:</label><br></br>
             <input type="text" name="auid"
             onChange={(e) => setAUID(e.target.value)}
-             required  className='border-2  border-slate-400 border-solid rounded-md p-1.5'/>
+             required  className='border-2  border-slate-400 border-solid rounded-md p-1.5 px-8'/>
             <br />
             </div>
             <button type="submit"  className='border-[1px] rounded-2xl border-solid border-white bg-orange-700 p-2 text-white'>
             Start The Quize
             </button>
         </form>
+
+    </div>
+   
+       
     </section>
       
     </div>
